9033f481378d597fcedb05c8083cbb213fbe8b2a
[Adithya Hebbar] <hebbaradithya215@gmail.com>
2024-09-08 14:19:52 +0530

    model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_training import ModelTrainingConfig
+from src.components.model_training import ModelTraining
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifact','train.csv')
@@ -41,3 +47,12 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
         
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTraining()
+    print(modeltrainer.initiate_model_training(train_arr,test_arr))
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 import pickle
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -19,3 +20,20 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def evaluate(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+        
+        for model in models:
+            m=models[model]
+            m.fit(X_train,y_train)
+
+            prediction=m.predict(X_test)
+            test_accuracy=r2_score(y_test,prediction)
+
+            report[model]=test_accuracy
+
+        return report
+    except Exception as e:
+        raise CustomException(e,sys)
